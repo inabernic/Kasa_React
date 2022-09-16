456cddab18f64040a0d22d2b0e6d8a68f07861bb
inabernic <bernic.in@gmail.com>
2022-09-16 17:05:59 +0200

    about_page

--- projet/colors.js
+++ projet/colors.js
@@ -1,5 +1,5 @@
 const colors = {
-    primary: 'linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 100%);',
+    primary: '#FF6060',
     secondary: '#FFFFFF',
     gris: '#C4C4C4',
     footer: '#E5E5E5'
--- projet/src/pages/AboutPage/AboutPage.jsx
+++ projet/src/pages/AboutPage/AboutPage.jsx
@@ -1,19 +1,56 @@
-import React from 'react';
-import './AboutPage.css';
+import React from 'react'
+
+// STYLE
+import './AboutPage.css'
+
+import Accordion from '../AboutPage/components/Accordion'
 
 // ------- COMPONENTS -------------
-import Nav from '../../components/Nav/Nav';
+import Nav from '../../components/Nav/Nav'
 import Footer from '../../components/Footer/Footer'
 
-export default function About () {
-    return (
-        <div className='container_page_about'>
-            <Nav />
-        <div className="error">
-            <h1>L'mage</h1>
-            <p>Les tags</p>
+export default function About() {
+  return (
+    <div className="container_about-page">
+      <Nav />
+      <div className="body_about_page">
+        <div className="background_header_image">
+          <div className="header_img">
+            <img src="../about_header_image.png" alt="bgdimg" />
+          </div>
         </div>
-        <Footer/>
+
+        <div className="container_about_page">
+          <div className="accordion-container">
+            <Accordion
+              title="Fiabilité"
+              content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper..."
+            />
+          </div>
+
+          <div className="accordion-container">
+            <Accordion
+              title="Respect"
+              content=" Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper..."
+            />
+          </div>
+
+          <div className="accordion-container">
+            <Accordion
+              title="Service"
+              content=" Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper..."
+            />
+          </div>
+
+          <div className="accordion-container">
+            <Accordion
+              title="Sécurité"
+              content=" Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper..."
+            />
+          </div>
         </div>
-    );
+      </div>
+      <Footer />
+    </div>
+  )
 }
